cb59a919893f4e347a5279e73250a3333a4caa85
yangshidie <yangshidie@outlook.com>
2017-03-16 04:06:18 -0700

    add the English

--- cdn/Init.h
+++ cdn/Init.h
@@ -37,41 +37,41 @@ struct v_consum
 
 class GraphLinkedTable
 {
-private:
-	vector<v_net> vertex;
-	vector<v_consum> consume;
-	int v_count;               //Net Node Num
-	int e_count;               //Edge Num
-	int c_count;               //Consumer Node Num
-	int ServerCost;            //Sver Cost
-protected:
-	vector<edgev_v> vec_edge;      //All the Edge Infomation
-	vector<int> headEdge;           //first Edge No of Vertex
-	int gHead[MAX_NODE_NUM];
-	int gEdgeCount;            //edge No increase
-public:
-	GraphLinkedTable();
-	~GraphLinkedTable(){}
-	void SetGraphPara(int v_n, int e_n, int c_n,int sc);  
-	void InsertEdge(int v1, int v2, int b, int c);       //insert one edge information
-	void InsertEdgev_v(int v1, int v2, int b, int c);       //insert all edges infomation
-	void InsertConsume(int c, int v, int dem);           //insert consumer node information
-	void PrintNetListInfo();                             //print net node information
-	void  PrintConNodeInfo();                             //print consumer node information
-	void PrintEdgeInfo();                                 //print all edges' information
-	int GetEdgebw(int v1, int v2);                       //return bandwith between two nodes,if not exist,return 0
-	int GetEdgecost(int v1, int v2);                     //rreturn cost between two nodes,if not exist,return 0
-	vector<int> GetNeiborNode(int v);                    //return nextNodes
-	int GetConLinkNode(int cv);                          //return the net node id connected with consumer node,,if the id is not exist,return infinity
-	int GetConDemand(int cv);                            //return demand ofconsumer node,if the id is not exist,return infinity
-	int GetServerCost();                                 //return  Server Cost
-	int GetVNum();                                       //return the number of net node 
-	int GetCNum();                                       //return the number of consumer node
-	int GetVEdge(int v);                                 //Return Every node's degree
-	void GraphInit(char * topo[MAX_EDGE_NUM], int line_num);
-	stack<int> str_to_int(char *s);
-	int ReturnEdgeNo(int in,int out);                     //fanhui wangluojiedian d diyitiaobian bianhao
-	void PrintHeader();                                    //dayin suoyou jiedian de diyitiao bian bianhao
+	private:
+		vector<v_net> vertex;
+		vector<v_consum> consume;
+		int v_count;               //Net Node Num
+		int e_count;               //Edge Num
+		int c_count;               //Consumer Node Num
+		int ServerCost;            //Sver Cost
+	protected:
+		vector<edgev_v> vec_edge;      //All the Edge Infomation
+		vector<int> headEdge;           //first Edge No of Vertex
+		int gHead[MAX_NODE_NUM];
+		int gEdgeCount;            //edge No increase
+	public:
+		GraphLinkedTable();
+		~GraphLinkedTable(){}
+		void SetGraphPara(int v_n, int e_n, int c_n,int sc);  
+		void InsertEdge(int v1, int v2, int b, int c);       //insert one edge information
+		void InsertEdgev_v(int v1, int v2, int b, int c);       //insert all edges infomation
+		void InsertConsume(int c, int v, int dem);           //insert consumer node information
+		void PrintNetListInfo();                             //print net node information
+		void  PrintConNodeInfo();                             //print consumer node information
+		void PrintEdgeInfo();                                 //print all edges' information
+		int GetEdgebw(int v1, int v2);                       //return bandwith between two nodes,if not exist,return 0
+		int GetEdgecost(int v1, int v2);                     //rreturn cost between two nodes,if not exist,return 0
+		vector<int> GetNeiborNode(int v);                    //return nextNodes
+		int GetConLinkNode(int cv);                          //return the net node id connected with consumer node,,if the id is not exist,return infinity
+		int GetConDemand(int cv);                            //return demand ofconsumer node,if the id is not exist,return infinity
+		int GetServerCost();                                 //return  Server Cost
+		int GetVNum();                                       //return the number of net node 
+		int GetCNum();                                       //return the number of consumer node
+		int GetVEdge(int v);                                 //Return Every node's degree
+		void GraphInit(char * topo[MAX_EDGE_NUM], int line_num);
+		stack<int> str_to_int(char *s);
+		int ReturnEdgeNo(int in,int out);                     //fanhui wangluojiedian d diyitiaobian bianhao
+		void PrintHeader();                                    //dayin suoyou jiedian de diyitiao bian bianhao
 };
 
 
